fd4814d9680f3383158d2bfe777f81675db49cd6
Niraj Ganesh <niraj@2400-1A00-BD20.ip6.wlink.com.np>
2024-11-05 16:41:33 +0545

    initialized project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
